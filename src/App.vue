fb75cb3bbfb82175f172720cbb10a52d57afa3e2
lionelgwk <lionelgwk@icloud.com>
2022-10-17 18:05:50 +0800

    init files

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+
+  <h1>{{title}}</h1>
+    <p>Welcome!</p>
+    <div v-if="showModal">
+      <ModalOne theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">Register Here!</a>
+          <a href="">Get PYOLO Merch</a>
+        </template>
+        <h1>Sign up for Project YOLO!</h1>
+        <p>Get them by 12/12 for half price!</p>
+      </ModalOne>
+    </div>
+    <button @click ="toggleModal">Open Modal</button>
+
+    <div v-if="showModalTwo">
+      <ModalTwo @closeTheModal="toggleModalTwo">
+        <template v-slot:hyperlinks>
+          <a href="#">Github Repository</a>
+          <a href="#">My Instagram</a>
+        </template>
+        <h1>Are you ready for WAD2?</h1>
+        <p>Click here to visit the Github Repository!</p>
+      </ModalTwo>
+
+    </div>
+    <button @click="toggleModalTwo">Open Modal Two</button>
+    
+</template>
+
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+  import ModalOne from './components/ModalOne.vue'
+  import ModalTwo from './components/ModalTwo.vue'
+
+
+export default {
+  name: 'App',
+  components: { ModalOne, ModalTwo },
+  data (){
+    return {
+      title: 'welcome to vue motherfuckerssssss',
+      header: "Sign up for Project YOLO!",
+      text: "Get them by 12/12 for half price!",
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      return this.showModal = !this.showModal;
+    },
+    toggleModalTwo(){
+      return this.showModalTwo = !this.showModalTwo;
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/ModalTwo.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="backdrop" @click.self="closeModalTwo">
+        <div class="modal">
+            <slot></slot>
+            <div class="actions">
+                <slot name="hyperlinks"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        props: ['theme'],
+        methods :{
+            closeModalTwo(){
+                this.$emit('closeTheModal')
+            }
+        }
+    }
+
+</script>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    
+    .backdrop {
+        margin: 0;
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,.5);
+        width: 100%;
+        height: 100%;
+    }
+    
+    .modal h1 {
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    
+    .modal p {
+        font-style: normal;
+    }
+    
+    .modal .action {
+        text-align: center;
+        margin: 30px 0px 10px 0px;
+        color: #333;
+    }
+    
+    .modal .actions a{
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
+    }
+</style>
