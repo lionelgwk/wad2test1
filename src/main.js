03c4153bec5398ec586cedfc1a0abd63fc50c24b
lionelgwk <lionelgwk@icloud.com>
2022-10-27 22:52:08 +0800

    added about page info and aos

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import AOS from 'aos'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import 'aos/dist/aos.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(AOS.init()).mount('#app')
