45d844c8411de423265a7f34ce6bc2182478938a
lionelgwk <lionelgwk@icloud.com>
2022-10-28 01:28:00 +0800

    googlemaps api added as component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,27 @@
   <div class="home">
     <h1>PartyGoers</h1>
   </div>
+
+  <GoogleMap api-key="AIzaSyCTHZllCldMYoM9ByF8AcxKPWvIuFJsTx4" style="width: 100%; height: 500px" :center="center" :zoom="15">
+      <Marker :options="{ position: center }" />
+  </GoogleMap>
 </template>
 
 <script>
 // @ is an alias to /src
 
-export default {
-  name: 'HomeView',
-  components: {
-
-}
-}
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
+  
+  export default defineComponent({
+    name: 'HomeView',
+    components: { GoogleMap, Marker },
+    setup() {
+      const center = { lat: 40.689247, lng: -74.044502 };
+  
+      return { center };
+    },
+  });
 </script>
 
 <style scoped>
