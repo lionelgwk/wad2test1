7339506e55b40e3199688f758412d4baac6d2823
Ivan Yeo <ivan@Ivans-MacBook-Pro.local>
2022-10-30 14:51:44 +0800

    added location collection from firebase

--- src/fb.js
+++ src/fb.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import 'firebase/firestore'
+import {getFirestore, collection, getDocs} from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,9 +23,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const db = firebase.firestore();
+const db = getFirestore();
 db.settings({
     timestampsInSnapshots: true
 })
+// collection ref 
+const colRef = collection(db, 'locations');
+getDocs(colRef)
+  .then((snapshot) => {
+    let locations = []
+    snapshot.docs.forEach((doc) => {
+      locations.push({...doc.data(),id:doc.id})
+    })
+      console.log(locations)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
 
-export default db;
+// export default db;
