95ab54f88d92c2a50cd5a45fbb70b0a8987130ee
caitlinyap <101982705+caitlinyap@users.noreply.github.com>
2022-10-30 16:58:01 +0800

    homepage edit
    original homeview saved as homeview1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,66 @@
+<script setup>
+//Vue Material Kit 2 components
+
+// image
+const bgImage =
+  "https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80";
+</script>
 <template>
-  <div class="home">
-
-
-    <div class="page-header min-vh-80">
-      <!-- style="background-image: url('https://media.istockphoto.com/photos/festival-event-party-with-people-blurred-background-picture-id860440036?k=20&m=860440036&s=612x612&w=0&h=hmVVQMopocOcOy-HIYn3DBSJ6RxkPWTu0RuWdJF_w7w=')" -->
-    <span class="mask bg-gradient-dark opacity-6"></span>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 mx-auto">
-          <div class="text-center">
-            <h1><span class="party">Party</span>Goers</h1>
-            <h3 class="text-black">The best way to plan outings with your friends.</h3>
+  <header>
+    <div
+      class="page-header min-vh-100"
+      :style="{
+        backgroundImage: `url(${bgImage})`
+      }"
+      loading="lazy"
+    >
+      <div class="container">
+        <div class="row">
+          <div
+            class="col-lg-6 col-md-7 d-flex justify-content-center flex-column"
+          >
+            <h1 class="text-white text-center mb-4">Welcome to <span class="party">Party</span>Goers</h1>
+            <p class="text-white opacity-8 lead pe-5 me-5">
+              The best way to plan outings with your friends.
+            </p>
+            <!-- <div class="buttons">
+              <MaterialButton color="white" class="mt-4"
+                >Get Started</MaterialButton
+              >
+              <MaterialButton color="none" class="text-white shadow-none mt-4"
+                >Read more</MaterialButton
+              >
+            </div> -->
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <!-- <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
-    <div class="container">
-      <div class="section text-center">
-        <h2 class="title">Your main section here</h2>
-      </div>
-    </div>
-  </div> -->
-  </div>
-
-  
+  </header>
 </template>
+
+
 
 <script>
 // @ is an alias to /src
 
-
-
-
   
 export default {
-    name: 'HomeView',
-    components: {
-  }
+  name: 'HomeView',
+  components: {
+}
 }
 </script>
 
 <style scoped>
-  h1 {
-    margin-top: 60px;
-    color: black;
-    font-weight: bold;
-  }
+h1 {
+  margin-top: 60px;
+  color: black;
+  font-weight: bold;
+}
 
-  .party {
-    margin-top: 60px;
-    color: crimson;
-    font-weight: bold;
-  }
+.party {
+  margin-top: 60px;
+  color: crimson;
+  font-weight: bold;
+}
 </style>
