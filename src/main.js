9fda2a34669be8e6c67a067d852b07dc6f3b0cb0
caitlinyap <101982705+caitlinyap@users.noreply.github.com>
2022-11-01 14:59:38 +0800

    installed firebase

--- src/main.js
+++ src/main.js
@@ -10,6 +10,29 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import 'aos/dist/aos.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDug7cjjrHQnH2kXpXXNNmfAcNWzfy524E",
+  authDomain: "wad2test1.firebaseapp.com",
+  databaseURL: "https://wad2test1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wad2test1",
+  storageBucket: "wad2test1.appspot.com",
+  messagingSenderId: "1079852291342",
+  appId: "1:1079852291342:web:b9452aa94fc57c5ce2735f",
+  measurementId: "G-43TS1XSPJ9"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 let app = createApp(App);
 app.use(VueGeolocation); 
 app.use(GMaps, {
