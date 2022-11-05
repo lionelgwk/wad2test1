78a52a229bcc1c97a1fd76592fa08b8b7c614292
caitlinyap <101982705+caitlinyap@users.noreply.github.com>
2022-11-05 16:26:24 +0800

    my events
    @ ivan can u settle the firebase so we can create an account and input the events under it thank u

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -17,11 +17,31 @@
 
 </template>
 
-<script>
+<script setup>
 
 import AboutCarousel from '../components/AboutCarousel.vue'
 import AboutCard from '../components/AboutCard.vue'
+import { ref } from 'vue'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { useRouter } from 'vue-router' // import router
 
+const email = ref('')
+const password = ref('')
+const router = useRouter() // get a reference to our vue router
+const register = () => {
+    createUserWithEmailAndPassword(getAuth(),email.value, password.value) // need .value because ref()
+    .then((data) => {
+      console.log('Successfully registered!');
+      router.push('/feed') // redirect to the feed
+    })
+    .catch(error => {
+      console.log(error.code)
+      alert(error.message);
+    });
+}
+</script>
+
+<script>
 export default {
   components:{
     AboutCarousel, AboutCard
--- src/views/MyEvents.vue
+++ src/views/MyEvents.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="myevents">
+    <div class="myevents px-4"><br>
+    <br><br><br>
+      <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">CCA friends</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-danger stretched-link">View Party</a>
+  </div>
+</div>
     </div>
   </template>
   
