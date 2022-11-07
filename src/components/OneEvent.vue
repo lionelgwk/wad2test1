7e5661ef8375f97423118d72bf3ccda870179ed9
caitlinyap <101982705+caitlinyap@users.noreply.github.com>
2022-11-07 16:51:47 +0800

    creating a popup after party is selected

---
+++ src/components/OneEvent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="myevents px-4"><br>
+      <br><br>
+      <h1>Your parties</h1>
+      <div class="row">
+        <div class="col-lg-4 d-flex justify-content-center">
+          <div class="card my-2" style="width: 18rem;">
+          <div class="card-body">
+          <h5 class="card-title">CCA friends</h5>
+          <p class="card-text">19/11/2022</p>
+          <button @click="showModal = true" class="btn btn-danger">View Party</button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-4 d-flex justify-content-center">
+          <div class="card my-2" style="width: 18rem;">
+          <div class="card-body">
+          <h5 class="card-title">Classmates</h5>
+          <p class="card-text">27/11/2022</p>
+          <a href="#" class="btn btn-danger stretched-link">View Party</a>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-4 d-flex justify-content-center">
+          <div class="card my-2" style="width: 18rem;">
+          <div class="card-body">
+          <h5 class="card-title">Cousins</h5>
+          <p class="card-text">03/12/2022</p>
+          <a href="#" class="btn btn-danger stretched-link">View Party</a>
+          </div>
+          </div>
+        </div>
+    </div>
+  </div>
+  
+  <PopUp v-show="showModal"></PopUp>
+  </template>
+    <script>
+    // @ is an alias to /src
+  
+    import PopUp from '@/components/PopUp.vue'
+  
+    export default {
+      name: 'MyEvents',
+      components: {
+        PopUp  
+      },
+      data() {
+        return {
+          showModal: false
+        }
+      }
+    }
+    </script>
+    
+    
--- src/views/MyEvents.vue
+++ src/views/MyEvents.vue
@@ -1,23 +1,16 @@
 <template>
-    <div class="myevents px-4"><br>
-    <br><br><br>
-      <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">CCA friends</h5>
-    <p class="card-text"></p>
-    <a href="#" class="btn btn-danger stretched-link">View Party</a>
-  </div>
-</div>
-    </div>
-  </template>
-  
+<Event></Event>
+</template>
   <script>
   // @ is an alias to /src
-  
+
+  import Event from '@/components/OneEvent.vue';
+
   export default {
     name: 'MyEvents',
     components: {
-  }
+      Event  
+    }
   }
   </script>
   
