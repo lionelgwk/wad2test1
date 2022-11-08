66ef50052f73180b7124f987ff2c320936160d6d
lionelgwk <lionelgwk@icloud.com>
2022-11-08 16:41:33 +0800

    added registration system with sign in and sign out

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { getAuth, onAuthStateChanged } from "firebase/auth"; 
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import MyEvents from '../views/MyEvents.vue'
 import CreateEvent from '../views/CreateEvent.vue'
+import RegisterAccount from '../views/RegisterAccount.vue'
+import SignIn from '../views/SignIn.vue'
 
 const routes = [
   {
@@ -18,12 +21,25 @@ const routes = [
   {
     path: '/myevents',
     name: 'myevents',
-    component: MyEvents
+    component: MyEvents,
+    meta: {
+      requiresAuth: true,
+    }
   },
   {
     path: '/createevent',
     name: 'createevent',
     component: CreateEvent
+  },
+  {
+    path: '/registeraccount',
+    name: 'registeraccount',
+    component: RegisterAccount
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: SignIn
   }
 
 ]
@@ -32,5 +48,32 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
-
 export default router
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+}
+
+router.beforeEach(async(to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next();
+    }
+    else{
+      alert("No access!");
+      next("/");
+    }
+  }
+  else{
+    next();
+  }
+});
