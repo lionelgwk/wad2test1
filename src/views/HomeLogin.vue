af20f8752114347492452f20b67affc5ce0aa03f
Ivan Yeo <ivan@Ivans-MacBook-Pro.local>
2022-11-13 01:54:40 +0800

    login homepage responsive 1

--- src/views/HomeLogin.vue
+++ src/views/HomeLogin.vue
@@ -1,3 +1,4 @@
+
 <template>
 
       <!--     Fonts and icons     -->
@@ -16,17 +17,20 @@
  
 
   <link id="pagestyle" href="../assets/css/material-kit.css?v=3.0.4" rel="stylesheet" />
-
-  
+  <ResponsiveContainer
+  default-breakpoint="tiny"
+  :breakpoints="{small: 576, medium: 768, large: 992, xlarge:1200 }">
+ 
   <header class="header-2">
+  
     <div class="page-header min-vh-100"
       :style="{
         backgroundImage: 'https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
       }"
       loading="lazy">
-
-      <div class="home" data-aos="zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine"><br>
-    <h1>Welcome,<br><span class="party">{{name}}</span></h1>
+      
+      <div class="home"  data-aos="zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine"><br>
+    <h1><span class="party">Welcome,<br>{{name}}</span></h1>
 
       <p class="desc">We're here to make planning exciting<br> outings with your friends easier!</p><br>
       <p class="events">Your upcoming events:</p>
@@ -35,11 +39,16 @@
         <li>Secondary school reunion</li>
         <li>Family gathering</li>
       </ul><br><br>
+  
 
       <p class="click">Click on <router-link :to="{ name: 'myevents'}" class="text-danger">My Parties</router-link> to view more details.</p>
+      
   </div>
+
     </div>
   </header>
+
+</ResponsiveContainer>
 
 
 </template>
@@ -72,50 +81,53 @@ export default {
 <style scoped>
   h1 {
     padding-top: 110px;
-    font-size: 100px;
+    font-size: 400%;
     text-align: left;
-    padding-left:130px;
+    padding-left:10%;
     font-weight: bold;
     font-family: 'Varela Round', sans-serif;
 
   }
 
   .desc {
-    font-size: 20px;
+    font-size: 100%;
     text-align: left;
-    padding-left:130px;
+    padding-left:10%;
     font-family: 'Varela Round', sans-serif;
   }
 
   .events {
-    font-size: 40px;
+    font-size: 100%;
     text-align: left;
-    padding-left:130px;
+    padding-left:10%;
     font-weight: bold;
     font-family: 'Varela Round', sans-serif;
     color: crimson;
   }
 
   .eventlist {
-    font-size: 20px;
+    font-size: 100%;
     text-align: left;
-    padding-left:160px;
+    padding-left:15%;
     font-family: 'Varela Round', sans-serif;
   }
 
   .page-header{
     background-image: url('@/assets/partygoers.jpg');
     background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
     margin: auto;
-    height: 60vh;
+    height: 100%;
     width: 100%;
     font-family: 'Varela Round', sans-serif;
   }
 
   .click {
-    font-size: 20px;
+    font-size: 100%;
     text-align: left;
-    padding-left:130px;
+    padding-left:10%;
     font-family: 'Varela Round', sans-serif;
   }
 
